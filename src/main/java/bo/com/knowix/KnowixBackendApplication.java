2cf3a0f43c1f186d9c54d3b256f7b0ca40bff4e9
imferr <117535778+imferr@users.noreply.github.com>
2024-03-05 18:44:59 -0400

    creando el proyecto

---
+++ src/main/java/bo/com/knowix/KnowixBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.com.knowix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowixBackendApplication.class, args);
+	}
+
+}
