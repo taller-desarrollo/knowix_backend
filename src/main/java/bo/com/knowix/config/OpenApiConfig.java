8248ccc0daca21331f689259a8ae5492cf6f7b35
IsaiDiaz <jsoza057@gmail.com>
2024-04-25 10:01:55 -0400

    swagger configuration

---
+++ src/main/java/bo/com/knowix/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package bo.com.knowix.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Knowix API",
+                version = "1.0.0",
+                description = "API for Knowix application"
+        )
+)
+public class OpenApiConfig {
+
+}
