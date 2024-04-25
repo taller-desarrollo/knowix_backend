0c454dd69963b72cd5290ac7178faff23928014f
IsaiDiaz <jsoza057@gmail.com>
2024-04-25 10:12:05 -0400

    authentication bearer added to swagger

--- src/main/java/bo/com/knowix/config/OpenApiConfig.java
+++ src/main/java/bo/com/knowix/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package bo.com.knowix.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +18,18 @@ import org.springframework.context.annotation.Configuration;
                 title = "Knowix API",
                 version = "1.0.0",
                 description = "API for Knowix application"
-        )
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Bearer Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 
