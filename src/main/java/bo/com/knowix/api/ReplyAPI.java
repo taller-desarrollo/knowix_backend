0bbbf420d70ea89a04d701c1b07d6d1ffa15dfc8
imferr <117535778+imferr@users.noreply.github.com>
2024-05-15 09:13:27 -0400

    loggers to reply api

--- src/main/java/bo/com/knowix/api/ReplyAPI.java
+++ src/main/java/bo/com/knowix/api/ReplyAPI.java
@@ -16,8 +16,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.sql.Timestamp;
 
-import java.math.BigDecimal; 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.logging.Logger;
 
 import bo.com.knowix.bl.ReplyBL;
 import bo.com.knowix.dto.ReplyDTO;
@@ -26,12 +27,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api/v1/reply")
 public class ReplyAPI {
 
     private final ReplyBL replyBL;
+    private static final Logger LOGGER = Logger.getLogger(ReplyAPI.class.getName());
 
     @Autowired
     public ReplyAPI(ReplyBL replyBL) {
@@ -40,36 +41,64 @@ public class ReplyAPI {
 
     @GetMapping
     public ResponseEntity<List<ReplyDTO>> findAll() {
-        return new ResponseEntity<>(replyBL.findAll(), HttpStatus.OK);
+        LOGGER.info("Starting process to find all replies");
+        try {
+            LOGGER.info("Replies found successfully");
+            return new ResponseEntity<>(replyBL.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.warning("Error occurred while finding all replies: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            LOGGER.info("Finished process to find all replies");
+        }
     }
 
     // utilizando public ReplyDTO findById(int id) {:
     @GetMapping("/{id}")
     public ResponseEntity<ReplyDTO> findById(@PathVariable int id) {
-        return new ResponseEntity<>(
-                replyBL.findById(id),
-                HttpStatus.OK
-        );
+        LOGGER.info("Starting process to find reply by id");
+        try {
+            LOGGER.info("Reply found successfully");
+            return new ResponseEntity<>(replyBL.findById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.warning("Error occurred while finding reply by id: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            LOGGER.info("Finished process to find reply by id");
+        }
     }
 
-    // utilizando     public ReplyDTO findByPurchaseId(int purchaseId) {:
+    // utilizando public ReplyDTO findByPurchaseId(int purchaseId) {:
     @GetMapping("/purchase/{purchaseId}")
     public ResponseEntity<ReplyDTO> findByPurchaseId(@PathVariable int purchaseId) {
-        return new ResponseEntity<>(
-                replyBL.findByPurchaseId(purchaseId),
-                HttpStatus.OK
-        );
+        LOGGER.info("Starting process to find reply by purchase id");
+        try {
+            LOGGER.info("Reply found successfully");
+            return new ResponseEntity<>(replyBL.findByPurchaseId(purchaseId), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.warning("Error occurred while finding reply by purchase id: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            LOGGER.info("Finished process to find reply by purchase id");
+        }
     }
 
-    //utilizando     public ReplyDTO create(ReplyDTO replyDTO) {:
+    // utilizando public ReplyDTO create(ReplyDTO replyDTO) {:
     @PostMapping
     public ResponseEntity<ReplyDTO> create(@RequestBody ReplyDTO replyDTO) {
-        return new ResponseEntity<>(
-                replyBL.create(replyDTO),
-                HttpStatus.CREATED
-        );
+        LOGGER.info("Starting process to create reply");
+        try {
+            LOGGER.info("Reply created successfully");
+            return new ResponseEntity<>(replyBL.create(replyDTO), HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.warning("Error occurred while creating reply: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            LOGGER.info("Finished process to create reply");
+        }
     }
-
-
-
 }
