2184d22737b505f9f9c7bf6eb8a7c804560862bd
Moises Quispe <moises.quispe.arellano@gmail.com>
2024-05-16 09:04:41 -0400

    feat: KAN-220 send email when course payment is accepted

--- src/main/java/bo/com/knowix/bl/ReplyBL.java
+++ src/main/java/bo/com/knowix/bl/ReplyBL.java
@@ -1,9 +1,13 @@
 package bo.com.knowix.bl;
 
 import bo.com.knowix.dao.ReplyDAO;
+import bo.com.knowix.dao.repository.KcUserRepository;
+import bo.com.knowix.api.ContentReportAPI;
 import bo.com.knowix.dao.PurchaseDAO;
 import bo.com.knowix.dto.ReplyDTO;
 import bo.com.knowix.entity.ReplyEntity;
+import bo.com.knowix.service.EmailService;
+import bo.com.knowix.entity.KcUserEntity;
 import bo.com.knowix.entity.PurchaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +17,25 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import java.util.logging.Logger;
+
 @Service
 public class ReplyBL{
     private final ReplyDAO replyDAO;
     private final PurchaseDAO purchaseDAO;
 
+    private static final Logger LOGGER = Logger.getLogger(ContentReportAPI.class.getName());
+
     @Autowired
-    public ReplyBL(ReplyDAO replyDAO, PurchaseDAO purchaseDAO) {
+    private EmailService emailService;
+    @Autowired
+    private KcUserRepository kcUserRepository;
+
+    @Autowired
+    public ReplyBL(ReplyDAO replyDAO, PurchaseDAO purchaseDAO, EmailService emailService) {
         this.replyDAO = replyDAO;
         this.purchaseDAO = purchaseDAO;
+        this.emailService = emailService;
     }
 
     @Transactional
@@ -72,11 +86,32 @@ public class ReplyBL{
         replyEntity.setStatus(replyDTO.isStatus());
         replyEntity.setDate(replyDTO.getDate());
         replyEntity.setComent(replyDTO.getComent());
+
+        LOGGER.info("Finding purchase by id: " + replyDTO.getPurchaseId());
         Optional<PurchaseEntity> purchaseEntity = purchaseDAO.findById(replyDTO.getPurchaseId());
+
+
         if (purchaseEntity.isPresent()) {
+            LOGGER.info("Purchase found");
             replyEntity.setPurchase(purchaseEntity.get());
         }
+        else {
+            // TODO: control if its not present
+        }
+        
         replyDAO.save(replyEntity);
+
+        LOGGER.info("Finding user by id: " + purchaseEntity.get().getKcUserKcUuid());
+        KcUserEntity kcUser = kcUserRepository.findByKcUuid(purchaseEntity.get().getKcUserKcUuid());
+        if(kcUser == null) {
+            LOGGER.info("User not found");
+            throw new RuntimeException("User not found");
+        }
+        else {
+            LOGGER.info("User found, sending email to: " + kcUser.getEmail());
+            emailService.sendEmail(kcUser.getEmail(), "Tu compra del curso a sido " + replyDTO.getComent(), purchaseEntity.get().getCourse().toString());
+        }
+                
         return new ReplyDTO(
                 replyEntity.getReplyId(),
                 replyEntity.isStatus(),
